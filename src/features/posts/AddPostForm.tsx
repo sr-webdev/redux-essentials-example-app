8e688bc75119f6043caf513d5a47829a512567b3
sr-webdev <simonrio.webdev@gmail.com>
2026-02-26 14:23:35 -0600

    refactor: postAdded with prepare to initialize id

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -24,9 +24,7 @@ export const AddPostForm = () => {
     const title = elements.postTitle.value
     const content = elements.postContent.value
 
-    const newPost = { id: nanoid(), title, content }
-
-    dispatch(postAdded(newPost))
+    dispatch(postAdded(title, content))
 
     e.currentTarget.reset()
   }
