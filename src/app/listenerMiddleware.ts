ca2c59cc5293f9c6edd89822cdb451c25f18d00b
sr-webdev <simonrio.webdev@gmail.com>
2026-02-28 09:41:45 -0600

    feat: set up listenerMiddleware

---
+++ src/app/listenerMiddleware.ts
@@ -0,0 +1,11 @@
+import { createListenerMiddleware, addListener } from '@reduxjs/toolkit'
+import { AppDispatch, RootState } from './store'
+
+export const listenerMiddleware = createListenerMiddleware()
+
+export const startAppListening = listenerMiddleware.startListening.withTypes<RootState, AppDispatch>()
+
+export type AppStartListening = typeof startAppListening
+
+export const addAppListener = addListener.withTypes<RootState, AppDispatch>()
+export type AppAddListener = typeof addAppListener
